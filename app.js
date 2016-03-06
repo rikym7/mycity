d8f1a3669e57f54a32d9c359f21e0c3ee5dca854
rikym7 <rikym7@gmail.com>
2016-03-06 14:47:25 -0500

    My City Initial

---
+++ app.js
@@ -0,0 +1,17 @@
+var exp = require("express");
+var app = exp();
+
+var routes = require('./routes/routes.ejs');
+
+app.set ('view engine', 'ejs');
+
+app.get('/', routes.myHomeFn);
+app.get('/:city', routes.myCityFn);
+
+var port = process.env.POST || 4000;
+app.listen(port,function() {
+	console.log("server is listening on port " + port);
+
+});
+
+
