4f261bc89147e815460320559932bf3af766278d
rikym7 <rikym7@gmail.com>
2016-03-06 15:01:08 -0500

    Fixd PORT

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set ('view engine', 'ejs');
 app.get('/', routes.myHomeFn);
 app.get('/:city', routes.myCityFn);
 
-var port = process.env.POST || 8080;
+var port = process.env.PORT || 4000;
 app.listen(port,function() {
 	console.log("server is listening on port " + port);
 
